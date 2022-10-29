d61b059cf6796c2e21c3b4de9db8ef1cd7b89894
bumbleB <48389478+bumblebe-coding@users.noreply.github.com>
2022-10-30 00:25:14 +0530

    template for CP in C++

---
+++ solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//data types
+typedef long long ll;
+typedef long double lld;
+
+//constants
+const lld pi= 3.141592653589793238;
+const ll INF= LONG_LONG_MAX;
+const ll mod=1e9+7;
+
+//Macros
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define newl "\n";
+
+
+//=========================================================================================================
+
+void solve(){
+    
+}
+
+//=========================================================================================================
+
+//main function area
+int main(){
+    //faster input and output
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while (t--){
+        solve();
+   }
+}
